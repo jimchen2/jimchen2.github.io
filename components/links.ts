f0696b385db41c82170ef6f9b36c98500ba58840
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2024-07-19 19:03:24 +0800

    Update links.ts

--- components/links.ts
+++ components/links.ts
@@ -3,7 +3,7 @@ import { Link } from './types';
 export const links: Link[] = [
   { name: "jimchen.me", icon: "nextjs.png", url: "https://jimchen.me", category: "Apps" },
   { name: "Markdown Parser", icon: "nextjs.png", url: "https://markdown.jimchen.me", category: "Apps" },
-  { name: "Tasks (private)", icon: "nextjs.png", url: "https://task.jimchen.me", category: "Apps" },
+  { name: "Git Server", icon: "nextjs.png", url: "https://git.jimchen.me/explore/repos", category: "Apps" },
   { name: "Docker-Self-Hosted", icon: "docker.svg", url: "./self-hosted", category: "Apps" },
   { name: "Gmail", icon: "gmail.svg", url: "mailto:jimchen4214@gmail.com", category: "Social" },
   { name: "WeChat", icon: "wechat.svg", url: "./qr_code/wechat.jpeg", isQR: true, category: "Social" },
